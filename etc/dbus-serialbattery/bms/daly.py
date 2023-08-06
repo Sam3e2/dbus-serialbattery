93af36f718265b62c13141f2a3c4941d7019e3b6
Sam3e2 <98407597+Sam3e2@users.noreply.github.com>
2023-08-06 19:13:33 +0000

    test

--- etc/dbus-serialbattery/bms/daly.py
+++ etc/dbus-serialbattery/bms/daly.py
@@ -240,7 +240,7 @@ class Daly(Battery):
                     w = 2 * math.pi * dT / utils.AVERAGE_CURRENT_TIME_CONSTANT
                     a = w / (w + 1)
                     self.current = (1 - a) * self.current + a * current
-                
+                #test
                 self.soc = soc / 10
                 return True
 
